3ad33427f05fb715c3782b6904f606cff5e5d79a
uktechreviews <spencerorgan1@me.com>
2019-09-28 11:21:01 +0100

    Updated to add distance sensors

--- 1st attempt.py
+++ 1st attempt.py
@@ -1,1 +1,27 @@
 #Cristmas project - 1st attempt
+
+# import various GPIO devices
+
+from gpiozero import LED
+from gpiozero import Button
+from gpiozero import DistanceSensor
+import time
+
+# setup the devices
+
+led = LED(6)
+button = Button(3)
+
+led.off()
+
+sensor = DistanceSensor(echo=17, trigger=4)
+while True:
+    print('Distance: ', sensor.distance * 100)
+    if (sensor.distance*100) <=5:
+        led.on()
+        print("on")
+        time.sleep(1)
+    else:
+        led.off()
+        print("off")
+        time.sleep(1)
